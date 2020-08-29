ef6c5cbcae0354c6c1d9e0fea2cd39d931c126b0
Milgo <carolynemilgo@gmail.com>
2020-08-30 01:51:13 +0300

    Capitalize first letter of first word of phrase

---
+++ capitalize_first_letter.py
@@ -0,0 +1,7 @@
+#Capitalize first letter of first word of phrase.
+#Use function capitalize
+#only works if starting letter of phrase is a string else returns original phrase as is 
+
+def capitalize_first_letter(phrase):
+
+  return (phrase.capitalize())
