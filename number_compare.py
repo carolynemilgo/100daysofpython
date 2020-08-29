342f9e44d78d84eedb7017fd456fa27db33f75eb
Milgo <carolynemilgo@gmail.com>
2020-08-30 01:01:52 +0300

    Comparing numbers (>,< or =)

---
+++ number_compare.py
@@ -0,0 +1,9 @@
+# Report on whether a>b, b>a, or b==a
+
+def number_compare(a, b):
+    if a > b:
+      return("'a' is greater than 'b'")
+    elif b > a:
+      return("'b' is greater than 'a'")
+    else:
+       return("'a' is equal to 'b'") 
