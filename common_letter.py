11983de79b5f1015f2ef41e0ae88eaa42ef0f4f8
Milgo <carolynemilgo@gmail.com>
2020-09-01 23:35:45 +0300

    Return most common number in list

---
+++ common_letter.py
@@ -0,0 +1,13 @@
+
+#Return most-common number in list.
+
+
+def most_common(my_list):
+    counter = 0 #initialize counter
+    #loop through to get frequency of each element
+    for number in my_list:
+      frequency = my_list.count(number)
+      #if current frequency is > than counter, update our counter with the new frequency
+      if frequency > counter:
+        counter = frequency
+    return number
