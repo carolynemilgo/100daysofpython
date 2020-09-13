f9b7d88c89d52addc13359a70b10cc5c247ee614
Milgo <carolynemilgo@gmail.com>
2020-09-13 23:00:06 +0300

    Comparing numbers

--- number_compare.py
+++ number_compare.py
@@ -1,9 +1,15 @@
 # Report on whether a>b, b>a, or b==a
 
+
 def number_compare(a, b):
     if a > b:
-      return("'a' is greater than 'b'")
-    elif b > a:
-      return("'b' is greater than 'a'")
+        return " 'a' is greater than 'b' "
+    elif a < b:
+        return " 'b' is greater than 'a' "
     else:
-       return("'a' is equal to 'b'") 
+        return " 'a' is equal to 'b' "
+
+
+print(number_compare(4, 8))  # 'b' is greater than 'a'
+print(number_compare(8, 4))  #'a' is greater than 'b'
+print(number_compare(4, 4))  # 'a' is equal to 'b'
