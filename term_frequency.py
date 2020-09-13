82f547e3de9beedd9eb4bed2e0a34d8a4d8af68c
Milgo <carolynemilgo@gmail.com>
2020-09-13 23:17:39 +0300

    frequency of term in lst.

--- term_frequency.py
+++ term_frequency.py
@@ -1,6 +1,9 @@
-#Return frequency of term in lst.
+# Return frequency of term in lst.
+
 
 def frequency(lst, search_term):
-  return lst.count(search_term)
-    
-      
+    return lst.count(search_term)
+
+
+print(frequency(["apple", "pear", "banana", "pear"], "banana"))  # 1
+print(frequency(["apple", "pear", "banana", "pear"], "pear"))  # 2
