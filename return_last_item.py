aabbfbf5a61da4512c6fbd8c5cd827e3c369c2dc
Milgo <carolynemilgo@gmail.com>
2020-09-13 22:58:14 +0300

    Return last item in list

--- return_last_item.py
+++ return_last_item.py
@@ -1,9 +1,12 @@
-#Return last item in list (None if list is empty.)
-# In termninal, call function in this format
-# last_element([2,5,7,10]) if list of numbers 
+# Return last item in list (None if list is empty.)
+
 
 def last_element(lst):
-  if len(lst) == 0:
-    return None
-  else:
-    return lst[-1]
+    if len(lst) == 0:
+        return None
+    else:
+        return lst[-1]
+
+
+print(last_element([2, 5, 7, 9]))  # 9
+print(last_element([]))  # None
